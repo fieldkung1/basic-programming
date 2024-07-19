6945ec14955b96a18b26015a8d4036a055366022
fieldkung1 <kuramewazazx886@gmail.com>
2024-07-19 16:11:10 +0700

    เปรียบเทียบค่า

--- EP1 จุดเริ่มของความชิปหาย/index.ts
+++ EP1 จุดเริ่มของความชิปหาย/index.ts
@@ -6,3 +6,9 @@ let ต้องการกลับบ้านไหม: bolean = true
 console.log(fristName)
 console.log(Y)
 console.log(ต้องการกลับบ้านไหม)
+
+const a: number = 7
+const b: number = 4
+console.log(a === b)
+console.log(a > b)
+console.log(a <= b)
